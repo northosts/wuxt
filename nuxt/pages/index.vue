64a199c2efb739c688b89eae0b30521bf42b4c86
Oliwer Smedjevik <oliwer.smedjevik@gmail.com>
2019-05-03 00:42:27 +0200

    Removing requests from index page

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -4,31 +4,6 @@
 
 <script>
 export default {
-  async asyncData(context) {
-    const {
-      app: { $wp },
-      error
-    } = context
-
-    try {
-      const page = await $wp.frontPage().embed()
-      const menu = await $wp.menu().location('main')
-      const slug = await $wp
-        .slug()
-        .name('sample-page')
-        .embed()
-
-      return {
-        page,
-        menu,
-        slug
-      }
-    } catch (e) {
-      /* eslint-disable no-console */
-      console.log(e)
-      /* eslint-enable no-console */
-      error(e)
-    }
-  }
+  async asyncData(context) {}
 }
 </script>
