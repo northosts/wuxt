34b3a285ddb1e4748c14dc537a5a4bd971875a5b
Oliwer Smedjevik <oliwer.smedjevik@gmail.com>
2019-05-03 00:42:38 +0200

    Returning single object from slug endpoint instead of array

--- wp-content/themes/wuxt/api-extensions/slug.php
+++ wp-content/themes/wuxt/api-extensions/slug.php
@@ -7,7 +7,8 @@
 add_action('rest_api_init', 'wuxt_slug_route');
 
 
-function wuxt_slug_route() {
+function wuxt_slug_route()
+{
     register_rest_route('wuxt', '/v1/slug/(?P<slug>\S+)', array(
         'methods'  => 'GET',
         'callback' => 'wuxt_get_slug'
@@ -15,31 +16,30 @@ function wuxt_slug_route() {
 }
 
 
-function wuxt_get_slug( $object ) {
+function wuxt_get_slug($object)
+{
 
-    $slug = $object->get_param( 'slug' );
+    $slug = $object->get_param('slug');
 
-    $request = new WP_REST_Request( 'GET', '/wp/v2/posts' );
-    $request->set_param( 'slug', $slug );
+    $request = new WP_REST_Request('GET', '/wp/v2/posts');
+    $request->set_param('slug', $slug);
 
-    $response = rest_do_request( $request );
+    $response = rest_do_request($request);
 
-    if ( !$response->data ) {
+    if (!$response->data) {
 
-      $request = new WP_REST_Request( 'GET', '/wp/v2/pages' );
-      $request->set_param( 'slug', $slug );
+        $request = new WP_REST_Request('GET', '/wp/v2/pages');
+        $request->set_param('slug', $slug);
 
-      $response = rest_do_request( $request );
-
+        $response = rest_do_request($request);
     }
 
-    if ( !$response->data ) {
-       return new WP_Error( 'wuxt_no_such_slug', __( 'Slug does not exist' ), array( 'status' => 404 ) );
+    if (!$response->data) {
+        return new WP_Error('wuxt_no_such_slug', __('Slug does not exist'), array('status' => 404));
     }
 
-    $embed = $object->get_param( '_embed' ) !== NULL;
-    $data = rest_get_server()->response_to_data( $response, $embed );
+    $embed = $object->get_param('_embed') !== NULL;
+    $data = rest_get_server()->response_to_data($response, $embed);
 
-    return $data;
-
+    return $data[0];
 }
